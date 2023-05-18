a659efb90ea0ef784d044dc04eddd1434a520276
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-05-18 19:10:36 +0200

    Extracted Helperchecks to a dedicated file

--- src/Commands/ModerationSystem.cs
+++ src/Commands/ModerationSystem.cs
@@ -20,63 +20,7 @@ using Sentry;
 namespace AGC_Management.Commands.Moderation
 {
     public class ModerationSystem : BaseCommandModule
-    {
-        private static async Task<bool> CheckForReason(CommandContext ctx, string reason)
-        {
-            if (reason == null)
-            {
-                DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder().WithTitle("Fehler: Kein Grund angegeben!")
-                    .WithDescription("Bitte gebe einen Grund an")
-                    .WithColor(DiscordColor.Red);
-                    DiscordMessageBuilder msg = new DiscordMessageBuilder().WithEmbed(embedBuilder.Build()).WithReply(ctx.Message.Id, false);
-                    await ctx.Channel.SendMessageAsync(msg);
-
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-
-        private static string GenerateCaseID()
-        // Generate CaseID with mix from current time and random number
-        {
-            Random rnd = new Random();
-            string CaseID = DateTime.Now.ToString("yyyyMMddHHmmss") + rnd.Next(1000, 9999);
-            return CaseID;
-        }
-        public static async Task<bool> TicketUrlCheck(CommandContext ctx, string reason)
-        {
-            string TicketUrl = "modtickets.animegamingcafe.de";
-            Console.WriteLine($"Ticket-URL Check {reason}");
-            if (reason == null)
-            {
-                return false;
-            }
-            if (reason.ToLower().Contains(TicketUrl.ToLower()))
-            {
-                Console.WriteLine("Ticket-URL enthalten");
-                DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder().WithTitle("Fehler: Ticket-URL enthalten").
-                    WithDescription("Bitte schreibe den Grund ohne Ticket-URL").
-                    WithColor(DiscordColor.Red);
-                DiscordEmbed embed = embedBuilder.Build();
-                DiscordMessageBuilder msg_e = new DiscordMessageBuilder().WithEmbed(embed).WithReply(ctx.Message.Id, false);
-                await ctx.Channel.SendMessageAsync(msg_e);
-
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Keine URL gefunden");
-                return false;
-            }
-        }
-            
-
-
+    {     
         [Command("kick")]
         [RequirePermissions(Permissions.KickMembers)]
         public async Task KickMember(CommandContext ctx, DiscordMember member,[RemainingText] string reason)
