defe7e9eb9d4c9bd526f134ac93a144636ab1cea
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-05-21 20:10:23 +0200

    Created Inheritant Class from ModerationSystem

---
+++ src/Commands/ExtendedModerationSystem.cs
@@ -0,0 +1,11 @@
+using AGC_Management.Helpers;
+using DisCatSharp.Attributes;
+using DisCatSharp.CommandsNext;
+using DisCatSharp.CommandsNext.Attributes;
+
+namespace AGC_Management.Commands;
+
+class ExtendedModerationSystem : AGC_Management.Commands.ModerationSystem
+{
+    []
+}
