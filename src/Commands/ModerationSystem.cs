bc39e0b482243c90994ed5a04c208aea3e504113
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-05-24 00:11:08 +0200

    Reworked Ban command

--- src/Commands/ModerationSystem.cs
+++ src/Commands/ModerationSystem.cs
@@ -84,7 +84,7 @@ public class ModerationSystem : BaseCommandModule
     {
         if (await HelperChecks.CheckForReason(ctx, reason)) return;
         if (await HelperChecks.TicketUrlCheck(ctx, reason)) return;
-
+        var caseid = HelperChecks.GenerateCaseID();
         var embedBuilder = new DiscordEmbedBuilder()
             .WithTitle($"Du wurdest von {ctx.Guild.Name} gebannt!")
             .WithDescription($"**Begründung:**```{reason}```\n" +
@@ -99,57 +99,126 @@ public class ModerationSystem : BaseCommandModule
         string sentString;
         var username = user.UsernameWithDiscriminator;
 
-        try
+        // abfrage
+        var interactivity = ctx.Client.GetInteractivity();
+        var confirmEmbedBuilder = new DiscordEmbedBuilder()
+            .WithTitle("Überprüfe deine Eingabe").WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
+            .WithDescription($"Bitte überprüfe deine Eingabe und bestätige mit ✅ um fortzufahren.\n\n" +
+                             $"__Users:__\n" +
+                             $"```{user.UsernameWithDiscriminator}```\n__Grund:__```{reason}```")
+            .WithColor(GlobalProperties.EmbedColor);
+        var embed__ = confirmEmbedBuilder.Build();
+        List<DiscordButtonComponent> buttons = new(2)
         {
-            await user.SendMessageAsync(embed);
-            sent = true;
-            sentString = "Ja";
-        }
-        catch (Exception e)
+            new DiscordButtonComponent(ButtonStyle.Secondary, $"ban_accept_{caseid}", "✅"),
+            new DiscordButtonComponent(ButtonStyle.Secondary, $"ban_deny_{caseid}", "❌")
+        };
+        var confirmMessage = new DiscordMessageBuilder()
+            .WithEmbed(embed__).AddComponents(buttons).WithReply(ctx.Message.Id);
+        var confirm = await ctx.Channel.SendMessageAsync(confirmMessage);
+        var interaction = await interactivity.WaitForButtonAsync(confirm, ctx.User, TimeSpan.FromSeconds(60)); 
+        buttons.ForEach(x => x.Disable());
+        if (interaction.TimedOut)
         {
-            sentString = $"Nein. Fehlergrund: ```{e.Message}```";
-            sent = false;
-        }
-
-        var SentEmoji = sent ? "<:yes:861266772665040917>" : "<:no:861266772724023296>";
-
-        try
-        {
-            await ctx.Guild.BanMemberAsync(user.Id, 7, ReasonString);
-        }
-        catch (UnauthorizedException)
-        {
-            var failsuccessEmbedBuilder = new DiscordEmbedBuilder()
-                .WithTitle($"{user.UsernameWithDiscriminator} nicht gebannt")
-                .WithDescription(
-                    $"Der User ``{user.UsernameWithDiscriminator} ({user.Id})`` konnte nicht gebannt werden!\n\n")
-                .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
-                .WithColor(DiscordColor.Red);
-
-            var failsuccessEmbed = failsuccessEmbedBuilder.Build();
-            var failSuccessMessage = new DiscordMessageBuilder()
-                .WithEmbed(failsuccessEmbed)
-                .WithReply(ctx.Message.Id);
-
-            await ctx.Channel.SendMessageAsync(failSuccessMessage);
+            
+            var embed_ = new DiscordMessageBuilder()
+                .WithEmbed(confirmEmbedBuilder.WithTitle("Ban abgebrochen")
+                    .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
+                    .WithDescription(
+                        "Der Ban wurde abgebrochen.\n\nGrund: Zeitüberschreitung. <:counting_warning:962007085426556989>")
+                    .WithColor(DiscordColor.Red).Build());
+            await confirm.ModifyAsync(embed_);
             return;
         }
+        if (interaction.Result.Id == $"ban_deny_{caseid}")
+        {
+            await interaction.Result.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+            var embed_ = new DiscordMessageBuilder()
+                .WithEmbed(confirmEmbedBuilder.WithTitle("Ban abgebrochen")
+                    .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
+                    .WithDescription(
+                        "Der Ban wurde abgebrochen.\n\nGrund: Abgebrochen. <:counting_warning:962007085426556989>")
+                    .WithColor(DiscordColor.Red).Build());
+            await confirm.ModifyAsync(embed_);
+            return;
+        }
+        if (interaction.Result.Id == $"ban_accept_{caseid}")
+        {
+            await interaction.Result.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+            var loadingEmbedBuilder = new DiscordEmbedBuilder()
+                .WithTitle("Ban wird bearbeitet")
+                .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
+                .WithDescription("Der Ban wird bearbeitet. Bitte warten...")
+                .WithColor(DiscordColor.Yellow);
+            var loadingEmbed = loadingEmbedBuilder.Build();
+            var loadingMessage = new DiscordMessageBuilder()
+                .WithEmbed(loadingEmbed).AddComponents(buttons)
+                .WithReply(ctx.Message.Id);
+            await confirm.ModifyAsync(loadingMessage);
 
-        var successEmbedBuilder = new DiscordEmbedBuilder()
-            .WithTitle($"{user.UsernameWithDiscriminator} wurde erfolgreich gebannt")
-            .WithDescription(
-                $"Der User ``{user.UsernameWithDiscriminator} ({user.Id})`` wurde erfolgreich gebannt!\n\n" +
-                $"Grund: ``{reason}``\n\n" +
-                $"User wurde über den kick benachrichtigt? {SentEmoji}")
-            .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
-            .WithColor(DiscordColor.Green);
+            var b_users = "";
+            var n_users = "";
+            string e_string;
+            var ec = DiscordColor.Red;
+            DiscordMessage? umsg = null;
+            try
+            {
+                umsg = await user.SendMessageAsync(embed);
+                sent = true;
+            }
+            catch
+            {
+                sent = false;
+            }
 
-        var successEmbed = successEmbedBuilder.Build();
-        var SuccessMessage = new DiscordMessageBuilder()
-            .WithEmbed(successEmbed)
-            .WithReply(ctx.Message.Id);
+            var semoji = sent ? "<:yes:861266772665040917>" : "<:no:861266772724023296>";
+            try
+            {
+                await ctx.Guild.BanMemberAsync(user.Id, 7, ReasonString);
+                var dm = sent ? "✅" : "❌";
+                b_users += $"{user.UsernameWithDiscriminator} | DM: {dm}\n";
+            }
+            catch (UnauthorizedException)
+            {
+                n_users += $"{user.UsernameWithDiscriminator}\n";
+            }
+            if (n_users != "")
+            {
+                e_string = $"Der Ban war nicht erfolgreich.\n" +
+                           $"__Grund:__ ```{reason}```\n";
+                           e_string += $"__Nicht gebannte User:__\n" +
+                                       $"```{n_users}```";
+                ec = DiscordColor.Red;
+                if (sent)
+                {
+                    try
+                    {
+                        await umsg.DeleteAsync();
+                    }
+                    catch(Exception)
+                    {
+                        // ignored
+                    }
 
-        await ctx.Channel.SendMessageAsync(SuccessMessage);
+                }
+            }
+            else
+            {
+                e_string = $"Der Ban wurde erfolgreich abgeschlossen.\n" +
+                           $"__Grund:__ ```{reason}```\n" +
+                           $"__Gebannte User:__\n" +
+                           $"```{b_users}```";
+                ec = DiscordColor.Green;
+            }
+            var discordEmbedBuilder = new DiscordEmbedBuilder()
+                .WithTitle("Ban abgeschlossen")
+                .WithDescription(e_string)
+                .WithFooter(ctx.User.UsernameWithDiscriminator, ctx.User.AvatarUrl)
+                .WithColor(ec);
+            var discordEmbed = discordEmbedBuilder.Build();
+            await confirm.ModifyAsync(new DiscordMessageBuilder().WithEmbed(discordEmbed));
+        }
+        
     }
 
 
