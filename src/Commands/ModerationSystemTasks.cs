0016135b848888ca2f3a02cdc2db265ea02f58a9
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-05-25 12:50:35 +0200

    Updated Database Operations

--- src/Commands/ModerationSystemTasks.cs
+++ src/Commands/ModerationSystemTasks.cs
@@ -11,13 +11,17 @@ public class ModerationSystemTasks
     public async Task StartRemovingWarnsPeriodically(DiscordClient discord)
     {
         if (DatabaseService.IsConnected())
+        {
+            discord.Logger.LogInformation("Starte überprüfung auf abgelaufene Warns..");
             while (true)
             {
                 await RemoveWarnsOlderThan7Days(discord);
                 await Task.Delay(TimeSpan.FromMinutes(5));
             }
+        }
 
-        discord.Logger.LogWarning("Database not connected.. Disable Periodically Expired Warn check.");
+        discord.Logger.LogWarning(
+            "Datenbank nicht verbunden. Deaktiviere automatische überprüfung auf abgelaufene warns.");
     }
 
     private async Task RemoveWarnsOlderThan7Days(DiscordClient discord)
@@ -26,59 +30,39 @@ public class ModerationSystemTasks
         var warnlist = new List<dynamic>();
         int expireTime = (int)DateTimeOffset.UtcNow.AddSeconds(-604800).ToUnixTimeSeconds();
         string deleteQuery = $"DELETE FROM warns WHERE datum < '{expireTime}' AND perma = 'False'";
-        string selectQuery = $"SELECT * FROM warns WHERE datum < '{expireTime}' AND perma = 'False'";
 
-        await using (NpgsqlDataReader warnReader = DatabaseService.ExecuteQuery(selectQuery))
+        List<string> WarnQuery = new()
         {
-            while (warnReader.Read())
-            {
-                var warn = new
-                {
-                    UserId = warnReader.GetInt64(0),
-                    PunisherId = discord.CurrentUser.Id,
-                    Datum = warnReader.GetInt32(2),
-                    Description = warnReader.GetString(3) + " **[AUTO] EXPIRED WARN!**",
-                    Perma = warnReader.GetBoolean(4),
-                    CaseId = "EXPIRED-" + warnReader.GetString(5)
-                };
-                warnlist.Add(warn);
-            }
-        }
-
+            "*"
+        };
+        Dictionary<string, object> warnWhereConditions = new()
+        {
+            { "perma", false }
+        };
+        List<Dictionary<string, object>> WarnResults =
+            await DatabaseService.SelectDataFromTable("warns", WarnQuery, warnWhereConditions);
+        foreach (var result in WarnResults) warnlist.Add(result);
         foreach (var warn in warnlist)
         {
-            string insertQuery = "INSERT INTO flags (userid, punisherid, datum, description, caseid) VALUES " +
-                                 "(@UserId, @PunisherId, @Datum, @Description, @CaseId)";
-
-            await using (NpgsqlConnection connection = new(DatabaseService.GetConnectionString()))
+            Dictionary<string, object> data = new()
             {
-                if (connection.State != ConnectionState.Open)
-                    await connection.OpenAsync();
-
-                await using (NpgsqlCommand command = new(insertQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@UserId", warn.UserId);
-                    command.Parameters.AddWithValue("@PunisherId", warn.PunisherId);
-                    command.Parameters.AddWithValue("@Datum", warn.Datum);
-                    command.Parameters.AddWithValue("@Description", warn.Description);
-                    command.Parameters.AddWithValue("@CaseId", warn.CaseId);
-
-                    await command.ExecuteNonQueryAsync();
-                }
-            }
-
-            // Create a new connection for the delete query
-            await using (NpgsqlConnection connectionDelete = new(DatabaseService.GetConnectionString()))
-            {
-                if (connectionDelete.State != ConnectionState.Open)
-                    await connectionDelete.OpenAsync();
-
-                using (NpgsqlCommand commandDelete = new(deleteQuery, connectionDelete))
-                {
-                    int remWarn = await commandDelete.ExecuteNonQueryAsync();
-                    discord.Logger.LogInformation($"{remWarn} Abgelaufene Verwarnungen in Flags verschoben.");
-                }
-            }
+                { "userid", (long)warn["userid"] },
+                { "punisherid", (long)warn["punisherid"] },
+                { "datum", warn["datum"] },
+                { "description", "[AUTO] Warn Abgelaufen: " + warn["description"] },
+                { "caseid", "EXPIRED-" + warn["caseid"] }
+            };
+            await DatabaseService.InsertDataIntoTable("flags", data);
         }
+
+        Dictionary<string, (object value, string comparisonOperator)> whereConditions = new Dictionary<string, (object value, string comparisonOperator)>
+        {
+            { "datum", (expireTime, "<") },
+            { "perma", (false, "=") }
+        };
+
+        int rowsDeleted = await DatabaseService.DeleteDataFromTable("warns", whereConditions, "AND");
+
+        discord.Logger.LogInformation($"{rowsDeleted} Abgelaufene Verwarnungen in Flags verschoben.");
     }
 }
--- src/Services/DatabaseService.cs
+++ src/Services/DatabaseService.cs
@@ -187,4 +187,39 @@ public static class DatabaseService
 
         return results;
     }
+
+    public static async Task<int> DeleteDataFromTable(string tableName, Dictionary<string, (object value, string comparisonOperator)> whereConditions, string logicalOperator = "AND")
+    {
+        string deleteQuery = $"DELETE FROM \"{tableName}\"";
+
+        if (whereConditions != null && whereConditions.Count > 0)
+        {
+            string whereClause = string.Join($" {logicalOperator} ", whereConditions.Select(c => $"\"{c.Key}\" {c.Value.comparisonOperator} @{c.Key}"));
+            deleteQuery += $" WHERE {whereClause}";
+        }
+
+        int rowsAffected;
+
+        await using (NpgsqlConnection connection = new NpgsqlConnection(GetConnectionString()))
+        {
+            await connection.OpenAsync();
+
+            await using (NpgsqlCommand command = new NpgsqlCommand(deleteQuery, connection))
+            {
+                if (whereConditions != null && whereConditions.Count > 0)
+                {
+                    foreach (var condition in whereConditions)
+                    {
+                        NpgsqlParameter parameter = new NpgsqlParameter($"@{condition.Key}", condition.Value.value);
+                        command.Parameters.Add(parameter);
+                    }
+                }
+
+                rowsAffected = await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        return rowsAffected;
+    }
+
 }
