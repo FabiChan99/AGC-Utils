bcdb65e4f763bfa1e346ed28a04f3d3b3714008f
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-05-28 19:29:50 +0200

    Started Working on TempVC

--- src/Commands/TempVC/TempVoice.cs
+++ src/Commands/TempVC/TempVoice.cs
@@ -1,6 +1,21 @@
-﻿namespace AGC_Management.Commands.TempVC;
+﻿using AGC_Management.Helpers.TempVoice;
 
-public class TempVoice
+namespace AGC_Management.Commands.TempVC;
+
+public class TempVoice : TempVoiceHelper
 {
-    
+    private static List<ulong> LevelRoleIDs = new List<ulong>()
+    {
+        750402390691152005, 798562254408777739, 750450170189185024, 798555933089071154,
+        750450342474416249, 750450621492101280, 798555135071617024, 751134108893184072,
+        776055585912389673, 750458479793274950, 798554730988306483, 757683142894157904,
+        810231454985486377, 810232899713630228, 810232892386705418
+    };
+
+    private static List<string> lookup = new List<string>()
+    {
+        "5+", "10+", "15+", "20+", "25+", "30+", "35+", "40+", "45+", "50+", "60+", "70+", "80+", "90+", "100+"
+    };
+
+
 }
---
+++ src/Helpers/TempVoice/TempVoiceHelper.cs
@@ -0,0 +1,22 @@
+﻿using DisCatSharp.CommandsNext;
+
+namespace AGC_Management.Helpers.TempVoice;
+
+public class TempVoiceHelper : BaseCommandModule
+{
+    protected static string GetVCConfig(string key)
+    {
+        return BotConfig.GetConfig()["TempVC"][$"{key}"];
+    }
+
+
+    protected static async Task NoChannel(CommandContext ctx)
+    {
+        string errorMessage = $"<:attention:1085333468688433232> **Fehler!** " +
+                              $"Du besitzt keinen eigenen Kanal oder der Kanal gehört dir nicht. " +
+                              $"Wenn du keinen Kanal hast, kannst du einen unter <#{GetVCConfig("Creation_Channel_ID")}> erstellen.";
+
+        await ctx.Channel.SendMessageAsync(errorMessage);
+    }
+
+}
