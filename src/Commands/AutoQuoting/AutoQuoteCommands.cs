9387579ada734723e3992db955e17fd01a4f18dd
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-09-19 16:29:33 +0200

    Fixed permission

--- src/Commands/AutoQuoting/AutoQuoteCommands.cs
+++ src/Commands/AutoQuoting/AutoQuoteCommands.cs
@@ -1,10 +1,12 @@
 ﻿using DisCatSharp.CommandsNext;
 using DisCatSharp.CommandsNext.Attributes;
+using DisCatSharp.Enums;
 
 namespace AGC_Management.Commands.AutoQuoting;
 
 public class AutoQuoteCommands : BaseCommandModule
 {
+    [RequirePermissions(Permissions.Administrator)]
     [Command("autoquote")]
     public async Task AutoQuoteCommand(CommandContext ctx, bool active)
     {
