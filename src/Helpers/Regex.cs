fc21df97de926f986bc078b18cf5a748e40ac0af
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-09-28 01:01:26 +0200

    Added Regex and Extensionmethods as preperation for invite filtering

---
+++ src/Helpers/Regex.cs
@@ -0,0 +1,7 @@
+﻿namespace AGC_Management.Helpers
+{
+    public static class RegexPatterns
+    {
+        public const string INVITE = @"(?:https?://)?(?:(?:\w+\.))?discord(?:(?:app)?\.com/invite|\.gg)/(?<code>[a-z0-9-]+)(?:\?\S*)?(?:#\S*)?";
+    }
+}
