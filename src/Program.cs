ea2b802b8952d7310b83f77a3ea242527f279a7e
Fabi-Chan <fabi@fabi-chan.me>
2023-11-22 23:42:38 +0100

    Raised Version

--- src/Program.cs
+++ src/Program.cs
@@ -31,7 +31,7 @@ namespace AGC_Management;
 
 public class CurrentApplicationData
 {
-    public static string VersionString { get; set; } = "v1.40";
+    public static string VersionString { get; set; } = "v1.40.1";
     public static DiscordClient Client { get; set; }
 }
 
