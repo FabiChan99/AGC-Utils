d1037e6e3797bc050a561bd7666575ba5e148f0d
Fabi-Chan <fabi@fabi-chan.me>
2023-12-28 01:05:53 +0100

    Add ConsoleProgressBar utility class
    This new utility class, ConsoleProgressBar, provides a simple way to display a progress bar in the console. The class supports parameters for setting the total progress, bar size, and the character used to denote progress.

---
+++ src/Utils/ConsoleProgressbar.cs
@@ -0,0 +1,47 @@
+﻿public class ConsoleProgressBar
+{
+    private readonly int _total;
+    private int _current;
+    private readonly int _barSize;
+    private readonly char _progressCharacter;
+    private int _startCursorTop;
+
+    public ConsoleProgressBar(int total, int barSize = 50, char progressCharacter = '#')
+    {
+        _total = total;
+        _current = 0;
+        _barSize = barSize;
+        _progressCharacter = progressCharacter;
+        _startCursorTop = Console.CursorTop;
+    }
+
+    public void Increment()
+    {
+        _current++;
+        Draw();
+    }
+
+    private void Draw()
+    {
+        Console.SetCursorPosition(0, _startCursorTop);
+
+        if (_current < _total)
+        {
+            float progress = (float)_current / _total;
+            int progressWidth = (int)(_barSize * progress);
+
+            Console.Write("[");
+            Console.Write(new string(_progressCharacter, progressWidth));
+            Console.Write(new string('-', _barSize - progressWidth));
+            Console.Write("] ");
+
+            int percentage = (int)(progress * 100);
+            Console.Write($"{percentage}% Completed");
+        }
+        else
+        {
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, _startCursorTop);
+        }
+    }
+}
