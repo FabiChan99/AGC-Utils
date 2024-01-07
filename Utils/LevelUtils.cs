e12e2bec6954180097a7f08c742ebd6fc9fa8f60
Fabi-Chan <fabi@fabi-chan.me>
2024-01-07 17:48:40 +0100

    Add LevelUtils utility class
    This new class includes helper functions for level and XP computations. It calculates experience points (XP) required for a certain level, the level at given total XP and the remaining XP until the next level.

---
+++ Utils/LevelUtils.cs
@@ -0,0 +1,36 @@
+﻿namespace AGC_Management.Utils;
+
+public static class LevelUtils
+{
+    public static int XpForLevel(int lvl)
+    {
+        if (lvl <= 0)
+        {
+            return 0;
+        }
+        int alvl = lvl - 1;
+        return (int)(5 / 6.0 * (151 * alvl + 33 * Math.Pow(alvl, 2) + 2 * Math.Pow(alvl, 3)) + 100);
+    }
+
+    public static int LevelAtXp(int totalXp)
+    {
+        int level = 0;
+        int xpForNextLevel = 100;
+        
+        while (totalXp >= xpForNextLevel)
+        {
+            totalXp -= xpForNextLevel;
+            level++;
+            xpForNextLevel = 5 * (level * level) + (50 * level) + 100;
+        }
+
+        return level;
+    }
+
+    public static int XpUntilNextLevel(int xp)
+    {
+        int currentLevel = LevelAtXp(xp);
+        int xpForNextLevel = XpForLevel(currentLevel + 1);
+        return xpForNextLevel - xp;
+    }
+}
