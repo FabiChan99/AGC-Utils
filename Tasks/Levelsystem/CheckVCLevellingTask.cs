40bf7db816e4e9392ca58c3978266fafc712091e
Fabi-Chan <fabi@fabi-chan.me>
2024-01-07 02:40:54 +0100

    Add CheckVCLevellingTask in Levelsystem
    This commit introduces a new task named CheckVCLevellingTask in the Levelsystem namespace. This task, which includes functions for launching and initiating checks at specific intervals, helps in maintaining a consistent checking mechanism in the system.

---
+++ Tasks/Levelsystem/CheckVCLevellingTask.cs
@@ -0,0 +1,20 @@
+﻿namespace AGC_Management.Tasks.Levelsystem;
+
+public static class CheckVCLevellingTask
+{
+    public static async Task LaunchLoops()
+    {
+        await StartCheckVCLevelling();
+    }
+
+    private static async Task StartCheckVCLevelling()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        while (true)
+        {
+            // implemtation here
+
+            await Task.Delay(TimeSpan.FromMinutes(1));
+        }
+    }
+}
