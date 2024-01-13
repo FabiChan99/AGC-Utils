db210e26adee19dddb48aa8f39a6b02ef52b7668
Fabi-Chan <fabi@fabi-chan.me>
2024-01-13 10:05:26 +0100

    Add SkiaSharp package and improve user rank visuals
    This commit adds the SkiaSharp package to the project for image creation. It also significantly enhances the display of rank and experience point progression in the Rank command. This is achieved by creating a visual progress bar, detailing overall XP and user rank. Finally, the LevelUtils class has been expanded with various functions to better calculate user level and XP data.

--- Commands/Levelsystem/RankCommand.cs
+++ Commands/Levelsystem/RankCommand.cs
@@ -1,50 +1,58 @@
-﻿using AGC_Management.Utils;
+﻿using System.Drawing;
+using System.Net.Mime;
+using AGC_Management.Utils;
 using DisCatSharp.ApplicationCommands;
 using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.ApplicationCommands.Context;
+using System.Drawing;
+using SkiaSharp;
 
 namespace AGC_Management.Commands.Levelsystem;
 
 public class RankCommand : ApplicationCommandsModule
 {
     [SlashCommand("rank", "Zeigt den aktuellen Rang eines Nutzers an.")]
-    public static async Task Rank(InteractionContext ctx, [Option("user", "Der Nutzer von dem der Rang angezeigt werden soll.")] DiscordUser user = null)
+    public static async Task Rank(InteractionContext ctx, 
+        [Option("user", "Der Nutzer von dem der Rang angezeigt werden soll.")] DiscordUser user = null)
     {
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("<a:loading_agc:1084157150747697203> Rang wird geladen..."));
         if (user == null)
         {
             user = ctx.User;
         }
+        
         var embed = new DiscordEmbedBuilder
         {
             Title = "Rang von " + user.Username,
             Color = BotConfig.GetEmbedColor()
         };
+        
         var rank = await LevelUtils.GetRank(user.Id);
         var level = rank[user.Id].Level;
-        var xp = rank[user.Id].Xp;
-        var display_xp = Converter.FormatWithCommas(xp);
+        var totalxp = rank[user.Id].Xp;
+        var display_xp = Converter.FormatWithCommas(totalxp);
         var xpForCurrentLevel = LevelUtils.XpForLevel(level);
         var xpForNextLevel = LevelUtils.XpForLevel(level + 1);
         var xpForThisLevel = xpForNextLevel - xpForCurrentLevel;
-        // TODO: Add levelcompletionprogress bspw 58000 / 80620 (72%)
-        var xpForThisLevelUntilNow = xp - xpForCurrentLevel;
+        var xpForThisLevelUntilNow = totalxp - xpForCurrentLevel;
         var percentage = (int)(xpForThisLevelUntilNow / (float)xpForThisLevel * 100);
-        var percentageForProgressBar = (int)(percentage / 5.0);
-        string progressBar = "";
-        for (int i = 0; i < 20; i++)
-        {
-            if (i < percentageForProgressBar)
-            {
-                progressBar += "#";
-            }
-            else
-            {
-                progressBar += "-";
-            }
-        }
-        embed.AddField(new DiscordEmbedField("Level", level.ToString()));
-        embed.AddField(new DiscordEmbedField("XP", display_xp));
-        embed.AddField(new DiscordEmbedField("XP Fortschritt", progressBar));
-        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed));
+        var userRank = await LevelUtils.GetUserRankAsync(user.Id);
+        using var bar = ImageUtils.CreateProgressBar(200, 20, percentage / 100f, $"{percentage}%");
+        using var image = SKImage.FromBitmap(bar);
+        using var stream = image.Encode(SKEncodedImageFormat.Png, 100).AsStream();
+        stream.Position = 0;
+        
+        embed.WithTitle("Rang von " + user.Username);
+        embed.WithDescription($"**Level:** {level}\n" +
+                              $"**Rang:** {userRank}\n\n" +
+                              $"**Fortschritt:** {Converter.FormatWithCommas(xpForThisLevelUntilNow)} / {Converter.FormatWithCommas(xpForThisLevel)} **XP**\n" +
+                              $"**Gesamt XP:** {display_xp} XP");
+        embed.WithThumbnail(user.AvatarUrl);
+                              
+        
+        embed.WithImageUrl("attachment://progress.png");
+        
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed).AddFile("progress.png", stream));
     }
+
 }
--- Utils/LevelUtils.cs
+++ Utils/LevelUtils.cs
@@ -5,6 +5,11 @@ namespace AGC_Management.Utils;
 
 public static class LevelUtils
 {
+    /// <summary>
+    /// Calculates the experience points required to reach a given level.
+    /// </summary>
+    /// <param name="lvl">The level to calculate the experience points for.</param>
+    /// <returns>The experience points required to reach the given level.</returns>
     public static int XpForLevel(int lvl)
     {
         if (lvl <= 0)
@@ -15,12 +20,37 @@ public static class LevelUtils
         return (int)(5 / 6.0 * (151 * alvl + 33 * Math.Pow(alvl, 2) + 2 * Math.Pow(alvl, 3)) + 100);
     }
     
+    public static int XpForNextLevel(int lvl)
+    {
+        lvl += 1;
+        int alvl = lvl;
+        return (int)(5 / 6.0 * (151 * alvl + 33 * Math.Pow(alvl, 2) + 2 * Math.Pow(alvl, 3)) + 100);
+    }
+    
+    public static int XpToFinishLevel(int xp)
+    {
+        int level = LevelAtXp(xp);
+        int xpForNextLevel = XpForLevel(level + 1);
+        return xpForNextLevel - xp;
+    }
+    
+    
+    
+    /// <summary>
+    /// Calculates the minimum and maximum experience points (XP) required for a given level.
+    /// </summary>
+    /// <param name="lvl">The level.</param>
+    /// <returns>A dictionary containing the minimum and maximum XP values.</returns>
     public static Dictionary<int, int> MinAndMaxXpForThisLevel(int lvl)
     {
         int min = 0;
         int max = 0;
         if (lvl <= 0)
         {
+            if (lvl == 0)
+            {
+                return new Dictionary<int, int> { { 0, 100 } };
+            }
             return new Dictionary<int, int> { { min, max } };
         }
         int alvl = lvl - 1;
@@ -53,9 +83,13 @@ public static class LevelUtils
         }
         return progressBar;
     }
-    
-    
 
+
+    /// <summary>
+    /// Calculates the level based on the total experience points.
+    /// </summary>
+    /// <param name="totalXp">The total experience points.</param>
+    /// <returns>The level corresponding to the total experience points.</returns>
     public static int LevelAtXp(int totalXp)
     {
         int level = 0;
@@ -71,11 +105,43 @@ public static class LevelUtils
         return level;
     }
 
+    /// <summary>
+    /// Calculates the amount of experience points needed to reach the next level.
+    /// </summary>
+    /// <param name="xp">The current amount of experience points.</param>
+    /// <returns>The amount of experience points needed to reach the next level.</returns>
     public static int XpUntilNextLevel(int xp)
     {
         int currentLevel = LevelAtXp(xp);
         int xpForNextLevel = XpForLevel(currentLevel + 1);
         return xpForNextLevel - xp;
+    }
+
+    /// <summary>
+    /// Method to retrieve the ranking of a user based on their XP in the leveling data table. </summary>
+    /// <param name="userid">The unique identifier of the user.</param>
+    /// <returns>The rank of the user. Returns 0 if the user is not found.</returns>
+    public static async Task<int> GetUserRankAsync(ulong userid)
+    {
+        int rank = 0;
+        await using var db = new NpgsqlConnection(DatabaseService.GetConnectionString());
+        await db.OpenAsync();
+        await using var cmd = new NpgsqlCommand("SELECT userid, current_xp FROM levelingdata ORDER BY current_xp DESC", db);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        if (reader.HasRows)
+        {
+            while (await reader.ReadAsync())
+            {
+                rank++;
+                if (reader.GetInt64(0) == (long)userid)
+                {
+                    break;
+                }
+            }
+        }
+
+        await db.CloseAsync();
+        return rank;
     }
 
 
@@ -90,7 +156,7 @@ public static class LevelUtils
         await using var db = new NpgsqlConnection(DatabaseService.GetConnectionString());
         await db.OpenAsync();
 
-        var cmd = new NpgsqlCommand("SELECT current_xp FROM levelingdata WHERE userid = @id", db);
+        var cmd = new NpgsqlCommand("SELECT current_xp, current_level FROM levelingdata WHERE userid = @id", db);
         cmd.Parameters.AddWithValue("@id", (long)userId);
 
         await using var reader = await cmd.ExecuteReaderAsync();
@@ -99,7 +165,7 @@ public static class LevelUtils
             while (await reader.ReadAsync())
             {
                 var xp = reader.GetInt32(0);
-                var level = LevelAtXp(xp);
+                var level = reader.GetInt32(1);
                 rank[userId] = new RankData { Level = level, Xp = xp };
             }
         }
