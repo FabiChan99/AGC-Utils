3287d671d4c08397e15fc68221904c1f1119aa83
Fabi-Chan <fabi@fabi-chan.me>
2024-01-14 17:36:15 +0100

    Add setup command to level system settings

---
+++ Commands/Levelsystem/LevelsystemSettings/SetupCommand.cs
@@ -0,0 +1,13 @@
+﻿using DisCatSharp.ApplicationCommands.Attributes;
+
+namespace AGC_Management.Commands.Levelsystem;
+
+public partial class LevelSystemSettings
+{
+    [ApplicationCommandRequirePermissions(Permissions.Administrator)]
+    [SlashCommand("setup", "Richtet das Levelsystem ein", defaultMemberPermissions:(long)Permissions.Administrator)]
+    public static async Task SetupLevelcommand(CommandContext ctx)
+    {
+        // implement
+    }
+}
