045ff26cffd33a0de2a6c5a7b8b157ac03b0ebfe
Fabi-Chan <fabi@fabi-chan.me>
2024-01-14 22:04:07 +0100

    Add MessageFormatter utility class
    This update introduces a new utility class, MessageFormatter. This class includes a static method for formatting level-up messages, with options for mention of user and role, based on certain conditions.

---
+++ Utils/MessageFormatter.cs
@@ -0,0 +1,21 @@
+﻿namespace AGC_Management.Utils;
+
+public class MessageFormatter
+{
+    public static async Task<string> FormatLevelUpMessage(string message, bool isWithReward, DiscordUser user,
+        DiscordRole role = null)
+    {
+        var formattedMessage = message.Replace("{usermention}", user.Mention);
+        formattedMessage = formattedMessage.Replace("{username}", user.Username);
+        if (isWithReward)
+        {
+            if (role != null)
+            {
+                formattedMessage = formattedMessage.Replace("{rolemention}", role.Mention);
+                formattedMessage = formattedMessage.Replace("{rolename}", role.Name);
+            }
+        }
+
+        return formattedMessage;
+    }
+}
