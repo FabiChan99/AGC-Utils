ce55806c982a6135e797495af2083e3e60e143c5
Fabi-Chan <fabi@fabi-chan.me>
2024-02-01 19:16:16 +0100

    Add Application entity for apply system later
    This commit introduces the Application entity into the Entities folder. The entity includes fields like UserId, application text (BewerbungsText), and the applied position.

---
+++ Entities/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace AGC_Management.Entities;
+
+public class Application
+{
+    public ulong UserId { get; set; }
+    public string BewerbungsText { get; set; }
+    public string Position { get; set; }
+}
