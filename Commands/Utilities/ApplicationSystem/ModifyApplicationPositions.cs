193b6b5774057d82ad3f5574daf50a858e4f771b
Fabi-Chan <fabi@fabi-chan.me>
2024-02-03 15:36:52 +0100

    Add ModifyApplicationPositions command class
    This commit introduces a new command class, ModifyApplicationPositions in the ApplicationSystem. It contains commands for adding, removing, and modifying application positions. All these operations are intended to update the application positions within the database.

---
+++ Commands/Utilities/ApplicationSystem/ModifyApplicationPositions.cs
@@ -0,0 +1,27 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+
+namespace AGC_Management.ApplicationSystem;
+
+[SlashCommandGroup("modifyapplicationpositions", "Modify the application positions.", defaultMemberPermissions:(long)Permissions.Administrator)]
+public sealed class ModifyApplicationPositions : ApplicationCommandsModule
+{
+    [SlashCommand("add", "Add a new application position.")]
+    public async Task AddPosition(InteractionContext ctx, string positionName)
+    {
+        // Add the position to the database
+    }
+
+    [SlashCommand("remove", "Remove an application position.")]
+    public async Task RemovePosition(InteractionContext ctx, string positionName)
+    {
+        // Remove the position from the database
+    }
+
+    [SlashCommand("modifyapplystatus", "Modify an application position to be applicable.")]
+    public async Task ModifyApplyStatus(InteractionContext ctx, string positionName)
+    {
+        // Modify the position to be applicable
+    }
+}
