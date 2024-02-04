f860f672630f4a784127c111caf87d44582feaa7
Fabi-Chan <fabi@fabi-chan.me>
2024-02-04 15:39:02 +0100

    bump to v2.4.0

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace AGC_Management;
 
 public class CurrentApplication
 {
-    public static string VersionString { get; set; } = "v2.3.0 rev2";
+    public static string VersionString { get; set; } = "v2.4.0";
     public static DiscordClient DiscordClient { get; set; }
     public static DiscordGuild TargetGuild { get; set; }
     public static ILogger Logger { get; set; }
