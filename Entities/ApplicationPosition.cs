7ae33a0f56c1135f7c4e6468d1923a5a113870ac
Fabi-Chan <fabi@fabi-chan.me>
2024-02-04 03:37:03 +0100

    Add management interface for application position (unfinished).

---
+++ Entities/ApplicationPosition.cs
@@ -0,0 +1,8 @@
+﻿namespace AGC_Management.Entities;
+
+public class ApplicationPosition
+{
+    public string PositionName { get; set; }
+    public string PositionId { get; set; }
+    public bool IsApplicable { get; set; }
+}
