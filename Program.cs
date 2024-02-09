ef0c92a543804a02a78a7757956fea7b9f8b1379
Fabi-Chan <fabi@fabi-chan.me>
2024-02-09 22:10:15 +0100

    Update application version to v2.5.1

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace AGC_Management;
 
 public class CurrentApplication
 {
-    public static string VersionString { get; set; } = "v2.5.0";
+    public static string VersionString { get; set; } = "v2.5.1";
     public static DiscordClient DiscordClient { get; set; }
     public static DiscordGuild TargetGuild { get; set; }
     public static ILogger Logger { get; set; }
