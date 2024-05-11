5e0064b9372767d10e4fcadfed9c1380fb04d57f
Fabi-Chan <fabi@fabi-chan.me>
2024-05-11 11:57:27 +0200

    Add new AutoModAlert event listener
    Added a new event listener, AutoModAlert, to the bot config. This listener creates a message event that triggers an AutoMod alert. Alerts are only sent if AutoMod is active and the message is sent on the AutoMod channel.

---
+++ Eventlistener/AutoModAlert.cs
@@ -0,0 +1,34 @@
+﻿namespace AGC_Management.Eventlistener;
+
+
+[EventHandler]
+public class AutoModAlert : BaseCommandModule
+{
+    
+    String AutoModAlertChannelId = BotConfig.GetConfig()["AutoModNotify"]["AlertChannelId"];
+    String AutoModChannelId = BotConfig.GetConfig()["AutoModNotify"]["AutoModChannelId"];
+    Boolean AutoModAlertActive = bool.Parse(BotConfig.GetConfig()["AutoModNotify"]["AutoModAlertActive"]);
+    
+    [Event]
+    private Task MessageCreated(DiscordClient client, MessageCreateEventArgs args)
+    {
+        _ = Task.Run(async () =>
+        {
+            if (args.Channel.Id.ToString() != AutoModChannelId)
+            {
+                return;
+            }
+            if (!AutoModAlertActive)
+            {
+                return;
+            }
+            var embed = new DiscordEmbedBuilder();
+            embed.WithTitle("AutoMod Alert");
+            embed.WithDescription($"AutoMod Alert: {args.Message.Content}");
+            embed.WithColor(DiscordColor.Red);
+            var channel = await client.GetChannelAsync(ulong.Parse(AutoModAlertChannelId));
+            await channel.SendMessageAsync(embed: embed);
+        });
+        return Task.CompletedTask;
+    }
+}
