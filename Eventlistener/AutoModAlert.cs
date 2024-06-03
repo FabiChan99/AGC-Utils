1bf79e70dab7e1a3c254e9ebd2ee49d86e1bb23f
Fabi-Chan <fabi@fabi-chan.me>
2024-06-03 12:02:30 +0200

    Implement queue for sending AutoMod alerts
    Added a queueing system and a timer to the AutoModAlert class to manage sending alerts asynchronously. This should prevent potential spamming and throttling issues when multiple alerts are triggered concurrently. Re-organized the code in MessageCreated event to enqueue tasks in the new queue system instead of sending them instantly.

--- Eventlistener/AutoModAlert.cs
+++ Eventlistener/AutoModAlert.cs
@@ -1,47 +1,76 @@
-﻿namespace AGC_Management.Eventlistener;
-
-
-[EventHandler]
-public class AutoModAlert : BaseCommandModule
+﻿namespace AGC_Management.Eventlistener
 {
-    
-    String AutoModAlertChannelId = BotConfig.GetConfig()["AutoModNotify"]["AlertChannelId"];
-    String AutoModChannelId = BotConfig.GetConfig()["AutoModNotify"]["AutoModChannelId"];
-    Boolean AutoModAlertActive = bool.Parse(BotConfig.GetConfig()["AutoModNotify"]["AutoModAlertActive"]);
-    
-    [Event]
-    private Task MessageCreated(DiscordClient client, MessageCreateEventArgs args)
+    [EventHandler]
+    public class AutoModAlert : BaseCommandModule
     {
-        _ = Task.Run(async () =>
-        {
-            if (args.Channel.Id.ToString() != AutoModChannelId)
-            {
-                return;
-            }
-            if (!AutoModAlertActive)
-            {
-                return;
-            }
-            var embed = new DiscordEmbedBuilder();
-            embed.AddField(new DiscordEmbedField("channel", args.Channel.Mention));
+        private static readonly Queue<Func<Task>> sendQueue = new();
+        private static Timer timer;
 
-            foreach (var field in args.Message.Embeds[0].Fields)
+        String AutoModAlertChannelId = BotConfig.GetConfig()["AutoModNotify"]["AlertChannelId"];
+        String AutoModChannelId = BotConfig.GetConfig()["AutoModNotify"]["AutoModChannelId"];
+        Boolean AutoModAlertActive = bool.Parse(BotConfig.GetConfig()["AutoModNotify"]["AutoModAlertActive"]);
+
+        public AutoModAlert()
+        {
+            timer = new Timer(SendAlertFromQueue, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private static void SendAlertFromQueue(object state)
+        {
+            if (sendQueue.Count > 0)
             {
-                if (field.Name is "rule_name" or "decision_id" or "channel_id")
+                var taskFunc = sendQueue.Dequeue();
+                var task = taskFunc();
+                
+                task.ContinueWith(_ =>
                 {
-                    continue;
-                }
-                embed.AddField(field);
+                    if (sendQueue.Count > 0)
+                    {
+                        timer.Change(5000, Timeout.Infinite);
+                    }
+                    else
+                    {
+                        timer.Change(Timeout.Infinite, Timeout.Infinite);
+                    }
+                });
             }
-            
-            
-            
-            embed.WithTitle("AutoMod Alert");
-            embed.WithFooter("Author: " + args.Author.Username + $" {args.Author.Id}");
-            embed.WithColor(DiscordColor.Red);
-            var channel = await client.GetChannelAsync(ulong.Parse(AutoModAlertChannelId));
-            await channel.SendMessageAsync(embed: embed);
-        });
-        return Task.CompletedTask;
+        }
+
+        [Event]
+        private Task MessageCreated(DiscordClient client, MessageCreateEventArgs args)
+        {
+            if (args.Channel.Id.ToString() != AutoModChannelId || !AutoModAlertActive)
+            {
+                return Task.CompletedTask;
+            }
+
+            sendQueue.Enqueue(async () =>
+            {
+                var embed = new DiscordEmbedBuilder();
+                embed.AddField(new DiscordEmbedField("channel", args.Channel.Mention));
+
+                foreach (var field in args.Message.Embeds[0].Fields)
+                {
+                    if (field.Name is "rule_name" or "decision_id" or "channel_id")
+                    {
+                        continue;
+                    }
+                    embed.AddField(field);
+                }
+
+                embed.WithTitle("AutoMod Alert");
+                embed.WithFooter("Author: " + args.Author.Username + $" {args.Author.Id}");
+                embed.WithColor(DiscordColor.Red);
+                var channel = await client.GetChannelAsync(ulong.Parse(AutoModAlertChannelId));
+                await channel.SendMessageAsync(embed: embed);
+            });
+
+            if (sendQueue.Count == 1)
+            {
+                timer.Change(0, Timeout.Infinite);
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }
