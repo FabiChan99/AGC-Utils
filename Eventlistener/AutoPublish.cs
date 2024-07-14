502abf6e49d3dd19d60bba2701793cecfe6945c3
Fabi-Chan <fabi@fabi-chan.me>
2024-07-15 01:16:33 +0200

    Add AutoPublishing for FreeGames

---
+++ Eventlistener/AutoPublish.cs
@@ -0,0 +1,14 @@
+﻿namespace AGC_Management.Eventlistener;
+
+[EventHandler]
+public class AutoPublish : BaseCommandModule
+{
+    [Event]
+    public static async Task MessageCreated(DiscordClient client, MessageCreateEventArgs args)
+    {
+        if (args.Channel.Id == 802947905241481227) {
+            await args.Channel.CrosspostMessageAsync(args.Message);
+        }
+    }
+
+}
